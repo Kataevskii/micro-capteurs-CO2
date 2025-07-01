b68628d5553066e0e97c1ff196007fd72a7f517c
Mikhail Kataevskii <mikhail.kataevskii@etu.minesparis.psl.eu>
2025-07-01 10:08:25 +0200

    clarify code

--- raspberry/temp.py
+++ raspberry/temp.py
@@ -1,4 +1,3 @@
-# Complete Project Details: https://RandomNerdTutorials.com/raspberry-pi-dht11-dht22-python/
 # Based on Adafruit_CircuitPython_DHT Library Example
 
 import time
@@ -7,30 +6,21 @@ import adafruit_dht
 
 # Sensor data pin is connected to GPIO 4
 sensor = adafruit_dht.DHT22(board.D4)
-# Uncomment for DHT11
-#sensor = adafruit_dht.DHT11(board.D4)
 
 def get_temperature():
     try:
         # Print the values to the serial port
-        temperature = sensor.temperature
-        humidity = sensor.humidity
-        return temperature
-    except RuntimeError as error:
+        return sensor.temperature
+    except (RuntimeError, Exception) as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-    except Exception as error:
         sensor.exit()
-        raise error
+        return error
 
 def get_humidity():
     try:
         # Print the values to the serial port
-        humidity = sensor.humidity
-        return humidity
-    except RuntimeError as error:
+        return sensor.humidity
+    except (RuntimeError, Exception) as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-    except Exception as error:
         sensor.exit()
-        raise error
+        return error
